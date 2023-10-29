081a4059d4a4ce9b16fb2a354e770c3233b21548
Styopa Groznov <groznov3@gmail.com>
2023-10-29 13:59:23 +0300

    Update Player.cs

--- Unity.Merge2/Assets/_Project/Player.cs
+++ Unity.Merge2/Assets/_Project/Player.cs
@@ -2,6 +2,7 @@
 {
     public class Player
     {
+        public ulong Money { get; set; }
         public int CollectedItemsCount { get; set; }
         public float CooldownSpeedModifire { get; set; } = 1;
         public int SpawnItemMergeLevel { get; set; }
